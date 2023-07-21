3642d0d2877839f02cd58554aee63e862473b8dd
Hemant Kumar <91686097+HemantK29@users.noreply.github.com>
2023-07-21 23:31:20 +0530

    Add files via upload
    Fully Working Todo App

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-const userTask = document.getElementById("userTask");
-const addTask = document.getElementById("btn");
-const taskList = document.getElementById("task-list");
-const remBtn = document.getElementById("btnK");
+const form = document.querySelector(".userTask");
+let userInput = document.querySelector("#userTask");
+let taskLists = document.querySelector(".task-list");
 
-addTask.addEventListener("click", () => {
-    taskList.innerHTML += `<li><p>${userTask.value}<button class="remBtn" id="btnK">X<button></p></li>`;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (userInput.value === "") {
+    alert("Please write some tasks!");
+  } else {
+    let newTask = document.createElement("li");
+    newTask.innerHTML = `<p>${userInput.value}</p>
+        <span
+          ><button class="done btn">Done</button
+          ><button class="remove btn">Remove</button></span
+        >`;
+    taskLists.append(newTask);
+    userInput.value = "";
+  }
 });
 
+taskLists.addEventListener("click", (e)=>{
+  console.log(e.target.textContent);
+    if(e.target.textContent === "Done"){
+        let listElement = e.target.parentNode.parentNode;
+        listElement.style.textDecoration = "line-through";
+    }if(e.target.textContent === "Remove"){
+      let listElement = e.target.parentNode.parentNode;
+      listElement.remove();
+    }
+})
